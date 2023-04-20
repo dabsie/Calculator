41a726115eceb9a0194554fa78f5afd8c8a00a96
dabsie <polumordvinov3004@gmail.com>
2023-04-20 14:34:00 +0500

    Калькулятор
    Обычный калькулятор с адаптивностью для разных размеров и девайсов
    Но пока крашит

---
+++ Calculator Layout iOS13/ViewController.swift
@@ -0,0 +1,93 @@
+//
+//  ViewController.swift
+//  Calculator Layout iOS13
+//
+//  Created by Angela Yu on 01/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var numberFromScreen:Double = 0
+    var firstNum: Double  = 0
+    var mathSign:Bool = false
+    var operation:Int = 0
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func numbers(_ sender: UIButton) {
+        if mathSign == true{
+            label.text = String(sender.tag)
+            mathSign = false
+        }
+        else {
+            label.text = label.text! + String(sender.tag)
+        }
+        
+        numberFromScreen = Double(label.text!)!
+    }
+    
+    @IBAction func buttons(_ sender: UIButton) {
+        firstNum = Double(label.text!)!
+        
+        if label.text != "" && sender.tag != 11 && sender.tag != 12 && sender.tag != 17 {
+            if sender.tag == 10 { // процент
+                label.text = "%"
+            }
+            
+            else if sender.tag == 13 { // деление
+                label.text = "/"
+            }
+            else if sender.tag == 14 { // умножение
+                label.text = "x"
+            }
+            else if sender.tag == 15 { // минус
+                label.text = "-"
+            }
+            else if sender.tag == 16 { // плюс
+                label.text = "+"
+            }
+            
+            mathSign = true;
+            operation = sender.tag
+        }
+        else if sender.tag == 11 { // хз что
+            
+        }
+        else if sender.tag == 12 { // clear
+            label.text = "0"
+            firstNum = 0
+            numberFromScreen = 0
+            operation = 0
+        }
+        else if sender.tag == 17 { // равно
+            if operation == 13{
+                label.text = String(firstNum / numberFromScreen)
+            }
+            else if operation == 14{
+                label.text = String(firstNum * numberFromScreen)
+            }
+            else if operation == 15{
+                label.text = String(firstNum - numberFromScreen)
+            }
+            else if operation == 16{
+                label.text = String(firstNum + numberFromScreen)
+            }
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+}
